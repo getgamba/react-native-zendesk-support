ce04837f993a5b5d942b1d3130cbe28df94eeb13
ac_wada <wada.0523.m@gmail.com>
2021-10-26 23:48:46 +0900

    version up

--- android/src/main/java/com/robertsheao/RNZenDeskSupport/RNZenDeskSupportModule.java
+++ android/src/main/java/com/robertsheao/RNZenDeskSupport/RNZenDeskSupportModule.java
@@ -5,37 +5,33 @@
 
 package com.robertsheao.RNZenDeskSupport;
 
-import android.content.Intent;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.Promise;
-
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.WritableMap;
-import com.zendesk.sdk.feedback.ui.ContactZendeskActivity;
-import com.zendesk.sdk.requests.RequestActivity;
-import com.zendesk.sdk.support.SupportActivity;
-import com.zendesk.sdk.support.ContactUsButtonVisibility;
-import com.zendesk.sdk.model.access.AnonymousIdentity;
-import com.zendesk.sdk.model.access.Identity;
-import com.zendesk.sdk.model.request.CustomField;
-import com.zendesk.sdk.model.request.CreateRequest;
-import com.zendesk.sdk.network.impl.ZendeskConfig;
-import com.zendesk.sdk.network.RequestProvider;
 import com.zendesk.service.ErrorResponse;
 import com.zendesk.service.ZendeskCallback;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
+import zendesk.core.AnonymousIdentity;
+import zendesk.core.Zendesk;
+import zendesk.support.CreateRequest;
+import zendesk.support.CustomField;
+import zendesk.support.Request;
+import zendesk.support.RequestProvider;
+import zendesk.support.Support;
+import zendesk.support.request.RequestActivity;
 
 public class RNZenDeskSupportModule extends ReactContextBaseJavaModule {
   public RNZenDeskSupportModule(ReactApplicationContext reactContext) {
@@ -61,7 +57,7 @@ public class RNZenDeskSupportModule extends ReactContextBaseJavaModule {
     String appId = config.getString("appId");
     String zendeskUrl = config.getString("zendeskUrl");
     String clientId = config.getString("clientId");
-    ZendeskConfig.INSTANCE.init(getReactApplicationContext(), zendeskUrl, appId, clientId);
+    Zendesk.INSTANCE.init(getReactApplicationContext(), zendeskUrl, appId, clientId);
   }
 
   @ReactMethod
@@ -76,76 +72,81 @@ public class RNZenDeskSupportModule extends ReactContextBaseJavaModule {
       builder.withNameIdentifier(identity.getString("customerName"));
     }
 
-    ZendeskConfig.INSTANCE.setIdentity(builder.build());
+    Zendesk.INSTANCE.setIdentity(builder.build());
   }
 
-  @ReactMethod
-  public void showHelpCenterWithOptions(ReadableMap options) {
-    SupportActivityBuilder.create()
-            .withOptions(options)
-            .show(getReactApplicationContext());
-  }
-
-  @ReactMethod
-  public void showCategoriesWithOptions(ReadableArray categoryIds, ReadableMap options) {
-    SupportActivityBuilder.create()
-            .withOptions(options)
-            .withArticlesForCategoryIds(categoryIds)
-            .show(getReactApplicationContext());
-  }
-
-  @ReactMethod
-  public void showSectionsWithOptions(ReadableArray sectionIds, ReadableMap options) {
-    SupportActivityBuilder.create()
-            .withOptions(options)
-            .withArticlesForSectionIds(sectionIds)
-            .show(getReactApplicationContext());
-  }
-
-  @ReactMethod
-  public void showLabelsWithOptions(ReadableArray labels, ReadableMap options) {
-    SupportActivityBuilder.create()
-            .withOptions(options)
-            .withLabelNames(labels)
-            .show(getReactApplicationContext());
-  }
-
-  @ReactMethod
-  public void showHelpCenter() {
-    showHelpCenterWithOptions(null);
-  }
-
-  @ReactMethod
-  public void showCategories(ReadableArray categoryIds) {
-    showCategoriesWithOptions(categoryIds, null);
-  }
-
-  @ReactMethod
-  public void showSections(ReadableArray sectionIds) {
-    showSectionsWithOptions(sectionIds, null);
-  }
-
-  @ReactMethod
-  public void showLabels(ReadableArray labels) {
-    showLabelsWithOptions(labels, null);
-  }
+//  @ReactMethod
+//  public void showHelpCenterWithOptions(ReadableMap options) {
+//    SupportActivityBuilder.create()
+//            .withOptions(options)
+//            .show(getReactApplicationContext());
+//  }
+//
+//  @ReactMethod
+//  public void showCategoriesWithOptions(ReadableArray categoryIds, ReadableMap options) {
+//    SupportActivityBuilder.create()
+//            .withOptions(options)
+//            .withArticlesForCategoryIds(categoryIds)
+//            .show(getReactApplicationContext());
+//  }
+//
+//  @ReactMethod
+//  public void showSectionsWithOptions(ReadableArray sectionIds, ReadableMap options) {
+//    SupportActivityBuilder.create()
+//            .withOptions(options)
+//            .withArticlesForSectionIds(sectionIds)
+//            .show(getReactApplicationContext());
+//  }
+//
+//  @ReactMethod
+//  public void showLabelsWithOptions(ReadableArray labels, ReadableMap options) {
+//    SupportActivityBuilder.create()
+//            .withOptions(options)
+//            .withLabelNames(labels)
+//            .show(getReactApplicationContext());
+//  }
+//
+//  @ReactMethod
+//  public void showHelpCenter() {
+//    showHelpCenterWithOptions(null);
+//  }
+//
+//  @ReactMethod
+//  public void showCategories(ReadableArray categoryIds) {
+//    showCategoriesWithOptions(categoryIds, null);
+//  }
+//
+//  @ReactMethod
+//  public void showSections(ReadableArray sectionIds) {
+//    showSectionsWithOptions(sectionIds, null);
+//  }
+//
+//  @ReactMethod
+//  public void showLabels(ReadableArray labels) {
+//    showLabelsWithOptions(labels, null);
+//  }
 
   @ReactMethod
   public void callSupport(ReadableMap customFields) {
 
     List<CustomField> fields = new ArrayList<>();
 
-    for (Map.Entry<String, Object> next : customFields.toHashMap().entrySet())
-      fields.add(new CustomField(Long.parseLong(next.getKey()), (String) next.getValue()));
-
-    ZendeskConfig.INSTANCE.setCustomFields(fields);
+    ReadableMapKeySetIterator iterator = customFields.keySetIterator();
+    while (iterator.hasNextKey()) {
+      String key = iterator.nextKey();
+      String value = customFields.getString(key);
+      fields.add(new CustomField(Long.parseLong(key), value));
+    }
 
     Activity activity = getCurrentActivity();
 
     if(activity != null){
-      Intent callSupportIntent = new Intent(getReactApplicationContext(), ContactZendeskActivity.class);
-      callSupportIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      getReactApplicationContext().startActivity(callSupportIntent);
+      Intent requestActivityIntent = RequestActivity.builder()
+              .withRequestSubject("Android ticket")
+              .withCustomFields(fields)
+              .intent(activity);
+      requestActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      getReactApplicationContext().startActivity(requestActivityIntent);
     }
   }
 
@@ -167,7 +168,7 @@ public class RNZenDeskSupportModule extends ReactContextBaseJavaModule {
           Promise promise) {
     try {
       // Get an instance of the RequestProvider from the ZendeskConfig
-      RequestProvider provider = ZendeskConfig.INSTANCE.provider().requestProvider();
+      RequestProvider provider = Support.INSTANCE.provider().requestProvider();
 
       // Build the request object from the javascript arguments
       CreateRequest zdRequest = new CreateRequest();
@@ -175,19 +176,19 @@ public class RNZenDeskSupportModule extends ReactContextBaseJavaModule {
       zdRequest.setSubject(request.getString("subject"));
       zdRequest.setDescription(request.getString("requestDescription"));
 
-      ArrayList<Object> list = request.getArray("tags").toArrayList();
+      ReadableArray list = request.getArray("tags");
       List<String> tagsList = new ArrayList<>(list.size());
-      for (Object object : list) {
-        tagsList.add(object != null ? object.toString() : null);
+      for (int i = 0; i < list.size(); i++) {
+        tagsList.add(list.getString(i));
       }
 
       zdRequest.setTags(tagsList);
 
       innerPromise = promise;
       // Create thew ZendeskCallback.
-      ZendeskCallback<CreateRequest> callback = new ZendeskCallback<CreateRequest>() {
+      ZendeskCallback<Request> callback = new ZendeskCallback<Request>() {
         @Override
-        public void onSuccess(CreateRequest createRequest) {
+        public void onSuccess(Request createRequest) {
           Log.d(TAG, "onSuccess: Ticket created!");
 
           WritableMap map = Arguments.createMap();
@@ -195,7 +196,6 @@ public class RNZenDeskSupportModule extends ReactContextBaseJavaModule {
 
           map.putString("description", createRequest.getDescription());
           map.putString("id", createRequest.getId());
-          map.putString("email", createRequest.getEmail());
           map.putString("subject", createRequest.getSubject());
 
           request.putMap("request", map);
