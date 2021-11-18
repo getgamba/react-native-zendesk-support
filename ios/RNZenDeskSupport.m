390db6f3126750c8173fc54064f1563bc47caf0e
ac_wada <wada.0523.m@gmail.com>
2021-11-18 23:40:05 +0900

    画像選択時、テキスト部分が隠れるのでフルスクリーン対応

--- ios/RNZenDeskSupport.m
+++ ios/RNZenDeskSupport.m
@@ -55,6 +55,7 @@ RCT_EXPORT_METHOD(callSupport:(NSDictionary *)customFields) {
         UIViewController *requestController = [ZDKRequestUi buildRequestUiWith:@[config]];
         
         UINavigationController *navController = [[UINavigationController alloc]initWithRootViewController:requestController];
+        navController.modalPresentationStyle = UIModalPresentationFullScreen;
         [window.rootViewController presentViewController:navController animated:YES completion:nil];
     });
 }
